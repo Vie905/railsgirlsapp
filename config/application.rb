9aba40233c62c1db2d9a680e2e994491ec6aad4f
vie <vmalificent@gmail.com>
2023-12-02 16:16:27 +0000

    add logo to homepage

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Railsgirlsapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.paths << Rails.root.join("app", "assets", "img")
   end
 end
